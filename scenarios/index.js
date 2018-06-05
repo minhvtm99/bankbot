70cc35d2578f93cb960057013c2e99bd10c496a4
minhvtm99 <39894594+minhvtm99@users.noreply.github.com>
2018-06-05 09:52:55 +0700

    Add files via upload

---
+++ scenarios/index.js
@@ -0,0 +1,56 @@
+'use strict';
+var utils = require('../utils');
+
+class Scenario {
+  constructor(f) {
+    console.log('Scenario starting...');
+  }
+
+  processPostback(sender, postback, f) {
+    return new Promise((resolve, reject) => {
+      let buttons = '';
+      let text = '';
+      let data = '';
+
+      //
+      if (postback && postback.payload) {
+        console.log('postback.payload :' + postback.payload);
+      }
+    });
+  }
+
+  processMessage(sender, message, f, wit) {
+    return new Promise((resolve, reject) => {
+      let buttons = '';
+      let text = '';
+      let data = '';
+      
+    });
+  }
+
+  processQuickreply(sender, message, f, agenda) {
+    //console.log('processQuickreply WIT resp :');
+    let buttons = '';
+    let text = '';
+    let data = '';
+
+    if (message && message.quick_reply) {
+      let quickReply = message.quick_reply;
+         
+    }
+  }
+
+  processAttachment(sender, message, f) {
+    //console.log('processAttachment ');
+    let buttons = '';
+    let text = '';
+    let data = '';
+    let locType = 'ATM';
+
+    if (message && message.attachments) {
+      
+    }
+  }
+}
+
+module.exports = Scenario;
