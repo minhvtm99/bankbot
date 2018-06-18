47dfba31ef89588e9e7bf889d40b586130e8246d
minhvtm99 <minhvtm99@gmail.com>
2018-06-18 03:20:58 -0400

    s

--- scenarios/index.js
+++ scenarios/index.js
@@ -55,7 +55,7 @@ class Scenario {
       request(options, function (error, response, body) {
           if (error) throw new Error(error);
 
-      console.log("sssss" + body);
+      console.log("sssss" +JSON.stringify(body));
 });
 
    
