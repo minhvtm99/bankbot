eec76b4f82529052bbbdd96d680b1621a41db7d9
minhvtm99 <minhvtm99@gmail.com>
2018-06-25 00:42:31 -0400

    s

--- scenarios/index.js
+++ scenarios/index.js
@@ -1,4 +1,12 @@
 'use strict';
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://minhvtm201099:alexisozil99@ds117691.mlab.com:17691/bankbotdev";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
 
 //Get entities
 const firstEntity = (entities, name) => {
@@ -670,7 +678,6 @@ class Scenario {
     });
 
   }
-
 
 }
 
