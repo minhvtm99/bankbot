df2acf05f87022218eb4345f35a1a62d08ea8421
minhvtm99 <minhvtm99@gmail.com>
2018-06-27 22:48:21 -0400

    s

--- scenarios/index.js
+++ scenarios/index.js
@@ -4,8 +4,7 @@ function mongo() {
   var MongoClient = require('mongodb').MongoClient;
   var url = "mongodb://minhvtm201099:alexisozil99@ds117691.mlab.com:17691/bankbotdev";
 
-  MongoClient.connect(url, function(err, db) {
-    var option = { useNewUrlParser: true };
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db)  {
     if (err) throw err;
     console.log("Database created!");
     db.close();
