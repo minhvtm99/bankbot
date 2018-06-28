dc83f610222908574e86305e54f49afcd27d352a
minhvtm99 <minhvtm99@gmail.com>
2018-06-27 23:18:41 -0400

    s

--- scenarios/index.js
+++ scenarios/index.js
@@ -12,7 +12,7 @@ const firstEntity = (entities, name) => {
 
 function doConnect() {
   var MongoClient = require('mongodb').MongoClient;
-  var url = "mongodb://minhvtm201099:alexisozil99@ds117691.mlab.com:17691/bankbotdev";
+  var url = "mongodb://minhvtm99:alexisozil99@ds117691.mlab.com:17691/bankbotdev";
 
   MongoClient.connect(url, { useNewUrlParser: true }, function(err, db)  {
     if (err) throw err;
